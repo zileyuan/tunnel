23dc093fea3d77a8cab4c9e0444afe1d32923f96
Zachary <zileyuan@gmail.com>
2025-09-30 15:53:14 +0800

    增加UI和autoload的加载管理

---
+++ scenes/ui/start.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@onready var loading_manager = $"/root/LoadingManager"
+
+@onready var new_game_button = $CenterContainer/RootVBox/ButtonsVBox/NewGameButton
+@onready var load_game_button = $CenterContainer/RootVBox/ButtonsVBox/LoadGameButton
+@onready var quit_button = $CenterContainer/RootVBox/ButtonsVBox/QuitButton
+@onready var progress_bar = $CenterContainer/RootVBox/ProgressBar
+
+func _ready() -> void:
+	get_tree().paused = false;
+	set_process_input(true);
+	loading_manager.load_progress_updated.connect(_on_load_progress_updated)
+	loading_manager.load_finished.connect(_on_load_finished)
+	
+func _on_load_progress_updated(progress: float) -> void:
+	progress_bar.value = progress * 100
+
+func _on_load_finished() -> void:
+	new_game_button.disabled = false
+	load_game_button.disabled = false
+	quit_button.disabled = false
+	progress_bar.modulate.a = 0
+	loading_manager.enter_main()
+	
+func _on_new_game_button_pressed() -> void:
+	new_game_button.disabled = true
+	load_game_button.disabled = true
+	quit_button.disabled = true
+	progress_bar.modulate.a = 255
+	loading_manager.start_load_main()
+
+func _on_load_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit();
---
+++ scripts/autoload/loading_manager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+signal load_progress_updated(progress: float)
+signal load_finished()
+
+var loaded_scene: PackedScene = null
+var load_state := ResourceLoader.THREAD_LOAD_INVALID_RESOURCE
+var progress := []
+
+const MAIN_SCENE_PATH = "res://scenes/main/main.tscn"
+
+func start_load_main() -> void:
+	var err = ResourceLoader.load_threaded_request(MAIN_SCENE_PATH, "PackedScene")
+	if err != OK:
+		print("Failed to start threaded load: %s" % err)
+		return
+	loaded_scene = null
+	load_state = ResourceLoader.THREAD_LOAD_IN_PROGRESS
+	
+func _process(_delta: float) -> void:
+	if load_state == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+		load_state = ResourceLoader.load_threaded_get_status(MAIN_SCENE_PATH, progress)
+		if progress.size() > 0:
+			print("Loading progress: ", progress[0] * 100, "%")
+		match load_state:
+			ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+				if progress.size() > 0:
+					load_progress_updated.emit(progress[0])
+			ResourceLoader.THREAD_LOAD_LOADED:
+				loaded_scene = ResourceLoader.load_threaded_get(MAIN_SCENE_PATH)
+				load_finished.emit()
+			ResourceLoader.THREAD_LOAD_FAILED, ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
+				print("场景加载失败或资源无效!")
+		
+func enter_main():
+	if loaded_scene:
+		get_tree().change_scene_to_packed(loaded_scene)
+	else:
+		print("错误：场景尚未加载完成或加载失败。")
